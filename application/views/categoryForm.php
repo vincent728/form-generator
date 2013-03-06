896885061411167b71e2ac1bfed8900a7a47008f
Vincent David Namlanda <vincentdaudi@gmail.com>
2013-03-06 09:30:53 +0300

    more input types added in manipulation algorithm

--- application/views/categoryForm.php
+++ application/views/categoryForm.php
@@ -15,44 +15,63 @@
  */
 $this->load->view('header');
 $this->load->view('content');
+
 if ($results->num_rows() > 0) {
 
     $input_output = '';
     foreach ($results->result_array() as $value) {
 
         //check if input is more than one
-        
+
         $inputfield = '';
-        
+
         if ($value['no_input'] > 0) {
 
 
             ///check if it is textarea, select dropdown etc
-             $fieldTobegenerated = '';
+            $fieldTobegenerated = '';
 
             switch (strtolower($value['input_type'])) {
-                
-                case "file":
-                    $fieldTobegenerated = form_label($value['input_name']) . '<input type="file" name="file[]"/>';
+
+
+                case "fullname_input":
+
+                    $fieldTobegenerated = form_label($value['input_name']) . form_input(array('name' => 'name[]', 'value' => '', 'size' => '30'));
                     break;
+                case "input_phone":
+
+                    $fieldTobegenerated = form_label($value['input_name']) . form_input(array('name' => 'phone[]', 'value' => '', 'size' => '30'));
+                    break;
+                
+                case "contact_email":
+
+                    $fieldTobegenerated = form_label($value['input_name']) . form_input(array('name' => 'contact_email[]', 'value' => '', 'size' => '30'));
+                    break;
+                
                 case "textarea":
 
                     $fieldTobegenerated = form_label($value['input_name']) . form_textarea(array('name' => 'txtarea[]', 'cols' => '25', 'rows' => '3'));
                     break;
-                
+
+                case "images":
+                    $fieldTobegenerated = form_label($value['input_name']) . '<input type="file" name="images[]"/>';
+                    break;
+
+                case "file":
+                    $fieldTobegenerated = form_label($value['input_name']) . '<input type="file" name="file[]"/>';
+                    break;
+
                 case "select":
 
-                    $fieldTobegenerated = form_label($value['input_name']);
-                    $out='';
-                    $repeatselectrtesults=$this->dataFetcher->getAllrepeats();
-                    
-                    foreach ($repeatselectrtesults->result_array()as $rows){
-                        
-                        $out.='<option value="'.$rows['repeat_id'].'">'.$rows['events'].'</option>';
-                     
+                    $out = '';
+                    $repeatselectrtesults = $this->dataFetcher->getAllrepeats();
+
+                    foreach ($repeatselectrtesults->result_array()as $rows) {
+
+                        $out.='<option value="' . $rows['repeat_id'] . '">' . $rows['events'] . '</option>';
                     }
-                    
-                    $fieldTobegenerated = form_label($value['input_name']).'<select name="" class="events">'.$out.'</select><div class="events"></div>';
+
+                    $fieldTobegenerated = form_label($value['input_name']) . '<select name="" class="events">' . $out . '</select><div class="events_display"></div>';
 
                     break;
 
@@ -73,7 +92,7 @@ if ($results->num_rows() > 0) {
 
 
         $input_output.=form_fieldset() . '<ul>' . $inputfield . '' . '</ul>' . form_fieldset_close();
-    } echo form_open_multipart('formGenerator/insertFormData/', $data = array('id' => '', 'class' => '')) .'<h1>'.$category.'</h1>'. $input_output . form_submit(array('name' => 'submit', 'value' => 'submit')) . form_close();
+    } echo form_open_multipart('formGenerator/insertFormData/', $data = array('id' => '', 'class' => '')) . '<h1>' . $category . '</h1>' . $input_output . form_submit(array('name' => 'submit', 'value' => 'submit')) . form_close();
 } else {
     
 }
