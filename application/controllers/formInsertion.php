774e6ec5f0d72be4c7c72c79fadd65e672ccc623
vincent728 <daudivincent@yahoo.com>
2013-04-23 12:15:29 +0300

    db testing with a live server

--- application/controllers/formInsertion.php
+++ application/controllers/formInsertion.php
@@ -12,13 +12,26 @@ class FormInsertion extends CI_Controller {
 
         if ($this->input->post('submit')) {
 
+            
+            echo 'hahah';
+            exit;
             //load all the input names as per db
             $results = $this->datafetcher->listAllInputstypes();
             $fields_name = array();
             foreach ($results as $value) {
                 array_push($fields_name, $value->input_name);
             }
-            //check submitted fields
+            //check submitted fields matches the posted fields
+           // $fieldtypename = str_replace(' ', '', $inputname)
+          
+           foreach ($_POST as $value) {
+               
+             
+               
+           }
+           echo var_dump($_POST);
+            
+            
         } else {
             
         }
